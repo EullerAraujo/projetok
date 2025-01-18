328e589a77709a7a7953932845571cb2339c4d9a
EullerAraujo <euller.araujo235@gmail.com>
2025-01-18 00:14:54 -0300

    Adiciona controle de rotação completo do jogador
    - O jogador agora pode girar em todas as direções (eixos X e Y).
    - Implementado Cursor.lockState para esconder e bloquear o cursor.

--- ProjetoK/Assets/Script/MouseLook.cs
+++ ProjetoK/Assets/Script/MouseLook.cs
@@ -11,7 +11,7 @@ public class MouseLook : MonoBehaviour
     float xRotation;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
@@ -26,5 +26,7 @@ public class MouseLook : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90, 90);
 
         transform.localRotation = Quaternion.Euler(xRotation,0,0);
+
+        playerBody.Rotate(Vector3.up*mouseX);
     }
 }
